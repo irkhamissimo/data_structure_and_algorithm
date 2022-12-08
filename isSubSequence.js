c1d180cb0cf83c8abcf096e449e47f712c12154e
irkham <irkham.nurhadi.13@gmail.com>
2022-12-08 17:44:51 +0700

    typo

--- isSubSequence.js
+++ isSubSequence.js
@@ -18,11 +18,10 @@ function isSubsequence(str1, str2) {
   var j = 0;
   if (!str1) return true;
   while (j < str2.length) {
-   
     if (str2[j] === str1[i]) i++;
     if (i === str1.length) return true;
-, jnjKlkJIj   j++;
-  }break
+    j++;
+  }
   return false;
 }
 
